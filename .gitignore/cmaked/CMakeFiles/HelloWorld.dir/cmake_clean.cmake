b4a1aa329536574b736616855e8280edcae5bea9
Igor Kochugov <apnnek@gmail.com>
2026-09-05 20:02:34 +0300

    And Hello

---
+++ .gitignore/cmaked/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/link.d"
+  "CMakeFiles/HelloWorld.dir/main.cpp.o"
+  "CMakeFiles/HelloWorld.dir/main.cpp.o.d"
+  "HelloWorld"
+  "HelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Penis\n";
+    return 0;
+}
